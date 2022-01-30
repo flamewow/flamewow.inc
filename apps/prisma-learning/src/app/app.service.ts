d6688278ae9ac141e8a7a1b7f411a49bd574acbf
Ilya Moroz <ilya.moroz@voxsync.com>
2022-01-30 23:32:13 +0200

    added 2nd learning prj

---
+++ apps/prisma-learning/src/app/app.service.ts
@@ -0,0 +1,12 @@
+import { HashService } from '@flamewow.inc/hash';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  constructor(private hashService: HashService) {}
+  async getData(): Promise<{ message: string }> {
+    const password = await this.hashService.getHash('password');
+
+    return { message: `Welcome to prisma-learning! ${password}` };
+  }
+}
